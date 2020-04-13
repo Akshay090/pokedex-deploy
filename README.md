555ed3bb6e3e4c01165be55431bb899c5c75cf06
Akshay Ashok <aks28id@gmail.com>
2020-04-13 19:10:28 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Deployment page for [Pokedex-DL](https://github.com/Akshay090/Pokedex-DL) 
+
+## It classifies the awesome starters Pikachu, Charmander, Squirtle and bulbasaur.
+
+You can test your changes locally by installing Docker and using the following command:
+
+```
+docker build -t poke-image . && docker run --rm -it -p 5000:5000 poke-image
+```
+
+This is deployed on azure app service, with cd from master branch
+
+This repo is based on [fastai deployment template](https://github.com/render-examples/fastai-v3)
---
+++ app/static/client.js
@@ -0,0 +1,64 @@
+var el = x => document.getElementById(x);
+
+function showPicker() {
+  el("file-input").click();
+}
+
+function showPicked(input) {
+//   el("upload-label").innerHTML = input.files[0].name;
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    el("image-picked").src = e.target.result;
+    el("image-picked").className = "";
+  };
+  reader.readAsDataURL(input.files[0]);
+}
+
+function analyze() {
+  var fileInput = el("file-input");
+  var uploadFiles = fileInput.files;
+//   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+//   var isDefault = fileInput.dataset.default;
+
+  el("analyze-button").innerHTML = "Analyzing...";
+
+  var xhr = new XMLHttpRequest();
+  var loc = window.location;
+  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+    true);
+  xhr.onerror = function() {
+    alert(xhr.responseText);
+  };
+  xhr.onload = function(e) {
+    if (this.readyState === 4) {
+      var response = JSON.parse(e.target.responseText);
+      el("result-label").innerHTML = `You choosed ${response["result"]}`;
+
+    }
+    el("analyze-button").innerHTML = "Analyze";
+  };
+
+   var defaultImg = el("image-picked").src
+    var defaultFile = dataURLtoFile(defaultImg, 'pikachu.png')
+    var fileData = new FormData();
+    fileData.append("file", defaultFile);
+    // console.log(defaultFile)
+    xhr.send(fileData);
+
+}
+
+function dataURLtoFile(dataurl, filename) {
+
+    var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], filename, {type:mime});
+}
+
